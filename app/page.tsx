77b1f7dedce68ba1077dd64ed2189080ae260146
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2024-02-17 20:18:32 +0530

    configure shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className="h-screen">
       <UserButton afterSignOutUrl="/" />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab rerum debitis deserunt sequi molestias, aliquid ex amet tempora voluptates minima consequuntur cupiditate sapiente praesentium, maiores officiis veniam dignissimos. Molestias, neque.
     </div>
   );
 }
